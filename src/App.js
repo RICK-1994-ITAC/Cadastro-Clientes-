e69385418cbb2a0f4e4e9d00e2733d1cbe5a8fa6
Henrique Lopes <henrique94ita@gmail.com>
2025-11-18 06:42:54 -0300

    corrigindo ghpages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-   <BrowserRouter>
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element= {<Home />}/>
       <Route path="/create" element= {<Create />}/>
